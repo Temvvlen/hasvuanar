c0670db7cd8ec1c2910d1d4e28cb78e8488ac2da
Hasvuanar <45098435+Temvvlen@users.noreply.github.com>
2026-03-24 10:57:40 +0900

    due till 03/25 task

--- 260319/python/2588.py
+++ 260319/python/2588.py
@@ -3,7 +3,7 @@ b = int(input())
 
 step1 = a * (b % 10)         
 step2 = a * ((b // 10) % 10) 
-step3 = a * (b // 100)        
+step3 = a * (b // 100)         
 final = a * b               
 
 print(step1)
