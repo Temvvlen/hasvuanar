8aa98d1380c4aa9ec60dd5d377f5f6ba31a3f840
Hasvuanar <45098435+Temvvlen@users.noreply.github.com>
2026-03-26 10:41:43 +0900

    due till 03/29 task

--- 260324/boj/2523.py
+++ 260324/boj/2523.py
@@ -1,8 +1,5 @@
 n = int(input())
-
-for i in range(n): 
-    for j in range(i + 1):
-        print("*", end="")
-for i in range(n):
-    for j in range(n - i):
-        print("*", end="")
+for i in range(1, n + 1):
+    print('*' * i)
+for i in range(n - 1, 0, -1):
+    print('*' * i)
