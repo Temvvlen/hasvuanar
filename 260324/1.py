e8070b149c7dffb596714c4994a777f3f1eefe3a
Hasvuanar <45098435+Temvvlen@users.noreply.github.com>
2026-03-26 10:22:20 +0900

    due till 03/29 task

--- 260324/1.py
+++ 260324/1.py
@@ -2,7 +2,7 @@ import math
 
 target = int(input())
 
-for i in range(2, int(math.sqrt(target))):
+for i in range(2, int(math.sqrt(target))): 
     if(target % i == 0):
         print("It's not prime number")
         break
