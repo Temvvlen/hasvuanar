1fcea21a2cbbbc165f4fa29ee570bea0e7baed2c
Hasvuanar <45098435+Temvvlen@users.noreply.github.com>
2026-03-26 10:38:38 +0900

    due till 03/29 task

--- 260324/boj/2439.py
+++ 260324/boj/2439.py
@@ -1,8 +1,6 @@
 n = int(input())
 
-for i in range(n):
-    for j in range(n - i):
-        print(end=" ")
-    for j in range(i + 1):
-        print("*", end="") 
-    print()
+for i in range(1, n + 1):
+    spaces = ' ' * (n - i) 
+    stars = '*' * i 
+    print(spaces + stars)
