aab10f08ef0ff759c2814d69fcd22f8a2deb678b
Hasvuanar <45098435+Temvvlen@users.noreply.github.com>
2026-03-26 10:27:04 +0900

    due till 03/29 task

--- 260324/boj/11022.py
+++ 260324/boj/11022.py
@@ -2,4 +2,4 @@ t = int(input())
 
 for i in range(t):
     a, b = input().split()
-    print("Case #{I}: {A} + {B} = {S}".format(I = i + 1, A = int(a), B = int(b), S = int(a) + int(b)))
+    print("Case #{I}: {A} + {B} = {S}".format(I = i + 1, A = int(a), B = int(b), S = int(a) + int(b))) 
