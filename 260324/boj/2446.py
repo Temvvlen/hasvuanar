716dac873aacac715128414a63b556c7376bce0d
Hasvuanar <45098435+Temvvlen@users.noreply.github.com>
2026-03-26 10:27:55 +0900

    due till 03/29 task

--- 260324/boj/2446.py
+++ 260324/boj/2446.py
@@ -2,7 +2,7 @@ n = int(input())
 
 for i in range(n):
     for j in range(i):
-        print(end=" ")
+        print(end=" ") 
     for j in range((n - i) * 2 + 1):
         print("*", end="")
     print()
